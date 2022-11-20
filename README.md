a050085718b5593539c79a2765421e78b255d722
kdandiwala <kdandiwala@ucsd.edu>
2022-11-19 17:10:04 -0800

    Finished Lab

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+Krish Dandiwala
+
+1. Yes, I would place automated tests within a Github action that runs whenever code is pushed so that each new code addition is properly tested. That way, receiving immediate feedback from failed tests allows the developer to fix bugs in their new code more efficiently, early on in the development cycle. 
+2. No, I wouldn't use end to end testing for a single function because it would be unnecessary. E2E testing is meant for simulating user input from start to finish which might end up testing way more functionality than the intended single function. 
+3. No, I wouldn't use a unit test to test the message feature of a messaging application since it should be tested in relation to other aspects of the application such as delivery status, accuracy, and the intended recipient. 
+4. Yes, I would use a unit test to check the max length of a message as it is encapsulated and not reliant on any other feature of the application. It would execute quickly and changing the other-app features won't affect this unit test. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,73 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+//isPhoneNumber
+test('isPhoneNumber - 1T', () => {
+    expect(functions.isPhoneNumber('(636) 234-0060')).toBe(true);
+});
+test('isPhoneNumber - 2T', () => {
+    expect(functions.isPhoneNumber('353-532-9234')).toBe(true);
+});
+test('isPhoneNumber - 3F', () => {
+    expect(functions.isPhoneNumber('3B4-492-249F')).toBe(false);
+});
+test('isPhoneNumber - 4F', () => {
+    expect(functions.isPhoneNumber('111111111111111')).toBe(false);
+});
+
+//isEmail
+test('isEmail - 1T', () => {
+    expect(functions.isEmail('kdandiwala@ucsd.edu')).toBe(true);
+});
+test('isEmail - 2T', () => {
+    expect(functions.isEmail('tester@tester.gov')).toBe(true);
+});
+test('isEmail - 3F', () => {
+    expect(functions.isEmail('tester@ucsd')).toBe(false);
+});
+test('isEmail - 4F', () => {
+    expect(functions.isEmail('gmail.com@gmail.com')).toBe(false);
+});
+
+//isStrongPassword
+test('isStrongPassword - 1T', () => {
+    expect(functions.isStrongPassword('A1f_jep2iejr')).toBe(true);
+});
+test('isStrongPassword - 2T', () => {
+    expect(functions.isStrongPassword('g_1_3_ddd_g99gd')).toBe(true);
+});
+test('isStrongPassword - 3F', () => {
+    expect(functions.isStrongPassword('1Apddc44')).toBe(false);
+});
+test('isStrongPassword - 4F', () => {
+    expect(functions.isStrongPassword('00')).toBe(false);
+});
+
+//isDate
+test('isDate - 1T', () => {
+    expect(functions.isDate('11/19/2022')).toBe(true);
+});
+test('isDate - 2T', () => {
+    expect(functions.isDate('1/1/0001')).toBe(true);
+});
+test('isDate - 3F', () => {
+    expect(functions.isDate('11-19-22')).toBe(false);
+});
+test('isDate - 4F', () => {
+    expect(functions.isDate('November 19th, 2022')).toBe(false);
+});
+
+//isHexColor
+test('isHexColor - 1T', () => {
+    expect(functions.isHexColor('#FFF')).toBe(true);
+});
+test('isHexColor - 2T', () => {
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+});
+test('isHexColor - 3F', () => {
+    expect(functions.isHexColor('000FF')).toBe(false);
+});
+test('isHexColor - 4F', () => {
+    expect(functions.isHexColor('TEST')).toBe(false);
+});
